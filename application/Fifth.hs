feea82468365aa2d8a41a9d56f74053bc4a9b19d
kapkekes <86285206+kapkekes@users.noreply.github.com>
2021-12-25 13:48:43 +0700

    Fix iconv

--- application/Fifth.hs
+++ application/Fifth.hs
@@ -3,7 +3,7 @@
 module Fifth
     ( main
     ) where
-
+{-
 import qualified HTML                               ( getHandler
                                                     , wait
                                                     )
@@ -36,3 +36,22 @@ main = do
     let matches   =  scan headingRE content
     mapM_ (DT.IO.hPutStrLn outputHandler . head . snd) matches
     HTML.wait
+-}
+import qualified Data.ByteString.Lazy as L
+import qualified System.IO as S
+import qualified Codec.Text.IConv as IConv
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.Encoding as TE
+import qualified Data.Text.Lazy.IO as TLIO
+
+main :: IO ()
+main = do
+    openhandle <- S.openFile "test.txt" S.ReadMode
+    writehandle <- S.openFile "testUp.txt" S.WriteMode
+    S.hSetEncoding writehandle S.utf8
+    contents <- L.hGetContents openhandle
+    let convcont = IConv.convert "CP1251" "UTF-8" contents
+    let readystr = TE.decodeUtf8 convcont
+    TLIO.hPutStr writehandle (T.toUpper readystr)
+    S.hClose openhandle
+    S.hClose writehandle
